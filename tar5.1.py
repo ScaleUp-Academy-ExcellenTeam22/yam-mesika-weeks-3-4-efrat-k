e6508aec6254197c3e51faa8bfa9bbaa8b5437af
efrat <efratk464@gmail.com>
2022-03-14 13:55:12 +0200

    commit tar5.1

---
+++ tar5.1.py
@@ -0,0 +1,25 @@
+import glob
+
+
+def func(path):
+    listt = glob.glob(path + "\deep*.*")
+    return listt
+
+
+path = input("Enter path of folder: ")
+listt = func(path)
+flag = True
+if len(listt) == 2:
+    for i in range(len(listt)):
+        if not listt[i].split(".")[-1] in ['jpg', 'png']:
+            flag = False
+else:
+    flag = False
+if flag:
+    print("The folder contain two file\n")
+else:
+
+
+    print("The folder not contain two file\n")
+
+
