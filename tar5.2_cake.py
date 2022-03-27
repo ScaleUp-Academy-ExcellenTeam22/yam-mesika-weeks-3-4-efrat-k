fca3fe2e72ec6b3498d28aab7493faad96b4c45c
efrat <efratk464@gmail.com>
2022-03-27 22:59:02 +0300

    tar5.2_cake after update

--- tar5.2_cake.py
+++ tar5.2_cake.py
@@ -1,18 +1,23 @@
-def get_recipe_price(prices, optionals=None,**ingeduents):
+
+"""
+    @:param Ingredient list of quantities and increases as well as a list of ingredients that are not needed
+    @:return The cost of the recipe according to the quantities of the products
+"""
+def get_recipe_price(prices, optionals=None,**ingredients):
     cost = 0
     if not optionals == None:
         for i in optionals:
            del prices[i]
 
-    for val, key in prices.items():
-            cost += ingeduents[val]/100* prices[val]
 
+    l = [ingredients[val]/100* prices[val] for val, key in prices.items()]
+    cost = sum(l)
     return cost
 
 
 
 
-
-print(get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100))
-print(get_recipe_price({'chocolate': 18, 'milk': 8}, optionals = ['milk'], chocolate=300))
-print(get_recipe_price({}))
+if __name__ =="__main__":
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, chocolate=200, milk=100))
+    print(get_recipe_price({'chocolate': 18, 'milk': 8}, optionals = ['milk'], chocolate=300))
+    print(get_recipe_price({}))
